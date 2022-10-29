178adbecffa3ddf285d49163be8132abdf6d5bc9
root <root@DESKTOP-7PSGMAA.localdomain>
2022-10-29 23:02:07 +0300

    removed some redundant definitions

--- betterbash.c
+++ betterbash.c
@@ -35,11 +35,8 @@ void *start_thread(void *thread_arg);
 int start_process(char *command, char *argv[]);
 
 #define ANSI_COLOR_RED     "\x1b[31m"
-#define ANSI_COLOR_GREEN   "\x1b[32m"
 #define ANSI_COLOR_YELLOW  "\x1b[33m"
 #define ANSI_COLOR_BLUE    "\x1b[34m"
-#define ANSI_COLOR_MAGENTA "\x1b[35m"
-#define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 /*--------------------------------------------------- MAIN ---------------------------------------------------*/
